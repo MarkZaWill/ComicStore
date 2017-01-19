307513b184d383e5287022076748322f3f85620f
Mark Williams <Mark.Williams@nationwidestudios.com>
2017-01-19 14:54:10 -0600

    initial commit

---
+++ Scratch/src/Scratch/Models/Comic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scratch.Models
+{
+    public class Comic
+    {
+        public int ComicID { get; set; }
+        public string Company { get; set; }
+        public string Character { get; set; }
+        public int Issue { get; set; }
+
+        public int DorkID { get; set; }
+
+    
+
+    }
+}
