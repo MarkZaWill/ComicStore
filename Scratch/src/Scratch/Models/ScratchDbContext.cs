d0b50084e8ab1234b33c5d467616fad016982fab
Mark Williams <Mark.Williams@nationwidestudios.com>
2017-01-19 15:24:01 -0600

    Added the dbcontext and added Entity Framework to my program

---
+++ Scratch/src/Scratch/Models/ScratchDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Scratch.Models
+{
+    public class ScratchDbContext: DbContext
+    {
+        public DbSet<Comic> Comics { get; set; }
+        public DbSet<Dork> Dorks { get; set; }
+    }
+}
