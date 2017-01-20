f588cc082a62c559efbbea88b4c864535d57f0af
Mark Williams <Mark.Williams@nationwidestudios.com>
2017-01-20 14:51:05 -0600

    added packages, packages crashed program, took away packages, crashed program, am now getting interesting error in web.config that cannot be resolved

---
+++ Scratch/src/Scratch/Controllers/ComicalController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Scratch.Models;
+
+
+namespace Scratch.Controllers
+{
+    //[Route("api/email")]
+    //[Produces("application/json")]
+    public class ComicalController : Controller
+    {
+
+         ScratchDbContext _context = new ScratchDbContext();
+
+
+
+        [HttpGet("api/Dork/{DorkID}")]
+        public IActionResult Get(int DorkID)
+
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Dork dork = _context.Dorks.FirstOrDefault(m => m.DorkID == DorkID);
+
+            IQueryable Query = from d in _context.Dorks
+                               join c in _context.Comics
+                        on d.DorkID equals c.DorkID
+                               select new
+                               {
+                                   d.FirstName,
+                                   d.LastName,
+                                   c.Character,
+                                   c.Issue        
+                                   //shipDate,
+                                   //TrackingNumber
+                               };
+            return Ok(Query);
+
+        }
+
+        [HttpPost]
+
+
+        public async Task<IEnumerable<string>> Post(IQueryable Query)
+        {
+            var result = await GetExternalResponse();
+            string QueryString = Query.ToString();
+            return new string[] { result, QueryString };
+        }
+
+        static string _address = "http://abn-nws-ftp.studio.internal.com:8080/swagger-ui.html";
+        private async Task<string> GetExternalResponse()
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(_address);
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+    }
+}
+
+
