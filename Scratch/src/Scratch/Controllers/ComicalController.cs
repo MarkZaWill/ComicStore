00f52dd5a38f150df260d821588646cb610271bf
Mark Williams <Mark.Williams@nationwidestudios.com>
2017-01-30 14:34:34 -0600

    changed around comical controller to include way to send data to external service

--- Scratch/src/Scratch/Controllers/ComicalController.cs
+++ Scratch/src/Scratch/Controllers/ComicalController.cs
@@ -22,13 +22,13 @@ namespace Scratch.Controllers
     {
         string connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ComicDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         private ScratchDbContext context;
-         
+
         public ComicalController(ScratchDbContext context)
         {
             this.context = context;
         }
-       
-            
+
+
 
 
 
@@ -36,57 +36,65 @@ namespace Scratch.Controllers
         public IActionResult Get(int DorkID)
 
         {
-            
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            if (context == null){
+            if (context == null)
+            {
                 return NotFound();
             }
-            Dork dork = context.Dorks.FirstOrDefault(m => m.DorkID == DorkID);
-            string newDork = dork.ToString();
-            Post(newDork);
-            //IQueryable Query = from d in context.Dorks
-            //                   join c in context.Comics
-            //            on d.DorkID equals c.DorkID
-            //                   select new
-            //                   {
-            //                       d.FirstName,
-            //                       d.LastName,
-            //                       c.Character,
-            //                       c.Issue        
-            //                       //shipDate,
-            //                       //TrackingNumber
-            //                   };
+            //Dork dork = context.Dorks.FirstOrDefault(m => m.DorkID == DorkID)
+            var dorks = context.Dorks.Where(m => m.DorkID == DorkID);
+
+            var comics = context.Comics.Where(m => m.DorkID == DorkID);
+
+            IEnumerable dork = from d in dorks
+                               join c in comics
+                               on d.DorkID equals c.DorkID
+                               where d.DorkID == c.DorkID
+                               select new metaDork
+                               {
+                                   FirstName = d.FirstName,
+                                   LastName=d.LastName,
+                                   Character = c.Character,
+                                   Issue= c.Issue
+
+                               };
+            foreach (metaDork m in dork)
+            {
+                Post(m);
+            }
             return Ok(dork);
 
 
         }
 
-        [HttpPost]
-        //public IActionResult POST(int DorkID)
-        //{ 
 
-        public async Task<IEnumerable<string>> Post(string dork)
+
+        public async Task<Uri> Post(metaDork body)
         {
-            var result = await GetExternalResponse();
-            return  new string[] { result, dork };
+            HttpClient client = new HttpClient();
+            //new Uri needs to be the base address for the service attempting to be called.
+            client.BaseAddress = new Uri("");
+
+            //clears headers
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            //makes sure that the headers will except json format.
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            //change the path of Api to where you want the external service to be called
+            string pathOfApi = " ";
+            HttpResponseMessage response = await client.PostAsJsonAsync(pathOfApi, body);
+            response.EnsureSuccessStatusCode();
+
+            return response.Headers.Location;
         }
-
-        static string _address = "http://abn-nws-ftp.studio.internal.com:8080/swagger-ui.html";
-        private async Task<string> GetExternalResponse()
-        {
-            var client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync(_address);
-            var result = await response.Content.ReadAsStringAsync();
-            return result;
-        }
-    
-
-            }
-
-            }
+    }
+}
 
 
---
+++ Scratch/src/Scratch/Models/metaDork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scratch.Models
+{
+    public class metaDork
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Character { get; set; }
+        public int Issue { get; set; }
+    }
+}
